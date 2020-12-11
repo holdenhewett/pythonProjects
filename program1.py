72b59dd8dbc85fda616ee6a63f6e2e343345721b
Holden Hewett <33139395+holdenhewett@users.noreply.github.com>
2020-12-11 15:58:02 -0800

    more logic to age if statement

--- program1.py
+++ program1.py
@@ -6,11 +6,15 @@ myName = str.lower(input('What is your name?'))  #ask for user's name
 if myName == 'liz':
     print('Oh my hot mama!')
 else:
-    print('It is nice to meet you,', myName)
+    print('It is nice to meet you,', myName) 
 print('The length of your name is', str(len(myName)), 'characters long!')
-myAge = int(input('What is your age? '))
-if myAge > 50:
+myAge = int(input('What is your age? '))  #ask for user's age
+if myAge >= 99:
     print("Man, you're old as hell!\nCongrats!")
+elif myAge == 100
+    print('You are 100. Awesome')
+elif myAge > 100
+    print('You have to be a vampire or something')
 else:
     age50 = 50 - myAge
     print('In', str(int(age50)), 'years you will be 50!')
