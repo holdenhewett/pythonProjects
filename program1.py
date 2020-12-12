03ec8ae3fa01564ab904072e1090162fe769c87d
Holden Hewett <33139395+holdenhewett@users.noreply.github.com>
2020-12-11 16:01:24 -0800

    fixed syntax and logic  bugs in if statement

--- program1.py
+++ program1.py
@@ -9,11 +9,11 @@ else:
     print('It is nice to meet you,', myName) 
 print('The length of your name is', str(len(myName)), 'characters long!')
 myAge = int(input('What is your age? '))  #ask for user's age
-if myAge >= 99:
+if myAge <= 99:
     print("Man, you're old as hell!\nCongrats!")
-elif myAge == 100
+elif myAge == 100:
     print('You are 100. Awesome')
-elif myAge > 100
+elif myAge > 100:
     print('You have to be a vampire or something')
 else:
     age50 = 50 - myAge
