2a52ca8994da037bab0642e04057a6f03e89f12a
Holden Hewett <33139395+holdenhewett@users.noreply.github.com>
2020-12-17 21:23:09 -0800

    Start of asSimple network cevice inventory program

---
+++ Networking/Devices.py
@@ -0,0 +1,23 @@
+class Router:
+    '''Router Class'''
+    def __init__(self, model, swversion, ip_addr):
+        '''initialize values'''
+        self.model = model
+        self.swversion = swversion
+        self.ip_addr = ip_addr
+
+    def getdesc(self):
+        '''return a formatted description of the router'''
+        desc = f'Router Model         :{self.model}\n'
+               f'Software Version     :{self.swversion}\n'
+               f'Management IP Address:{self.ip_addr}'
+        return desc
+
+class Switch(Router):
+    '''Switch Class'''
+    def getdesc(self):
+        '''return a formatted description of the switch'''
+        desc = (f'Switch Model         :{self.model}\n'
+                f'Software Version     :{self.swversion}\n'
+                f'Management IP Address:{self.ip_addr}')
+        return desc
---
+++ Networking/newDevice.py
@@ -0,0 +1,5 @@
+import Devices
+
+rtr1 = Devices('Cisco ISR 4080', 'IOS 14.5.1', '10.10.250.250')
+
+print('Router1\n', rtr1.Devices.Router.desc(), '\n', sep='')
