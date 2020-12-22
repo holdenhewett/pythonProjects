0fd98eb1c9f1e302fb788c864ff659bcd924431c
Holden Hewett <hhewett@elevensoftware.com>
2020-12-22 14:22:04 -0800

    Add f-string formatting to output

--- ATBS/program1/program1.py
+++ ATBS/program1/program1.py
@@ -10,11 +10,9 @@ else:
 print('The length of your name is', str(len(myName)), 'characters long!')
 myAge = int(input('What is your age? '))  #ask for user's age
 if myAge <= 99:
-    print("Man, you're old as hell!\nCongrats!")
-elif myAge == 100:
-    print('You are 100. Awesome')
-elif myAge > 100:
-    print('You have to be a vampire or something')
-else:
     age50 = 50 - myAge
-    print('In', str(int(age50)), 'years you will be 50!')
+    print(f'In {age50} years you will be 50!')
+elif myAge >= 100:
+    print('You lived to be over 100! Awesome!')
+elif myAge > 150:
+    print('You have to be a vampire or something')
