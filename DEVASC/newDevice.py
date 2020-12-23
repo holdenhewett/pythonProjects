f5c3f66f3ea02388b7ddd96af0c266af1d5d47d9
Holden Hewett <hhewett@elevensoftware.com>
2020-12-22 16:31:02 -0800

    Fix newDevice formatting & Add Work Directory

--- DEVASC/newDevice.py
+++ DEVASC/newDevice.py
@@ -1,5 +1,7 @@
 import Devices
 
 rtr1 = Devices.Router('Cisco ISR 4080', 'IOS 14.5.1', '10.10.250.250')
+sw1 = Devices.Switch('Cisco Catalyst 3850', 'IOS 10.5.4', '10.10.250.251')
 
-print('Router1\n', rtr1.getdesc(), '\n', sep='')
+print(f'Router 1\n{rtr1.getdesc()}\n', sep='')
+print(f'Switch 1\n{sw1.getdesc()}',sep='')
